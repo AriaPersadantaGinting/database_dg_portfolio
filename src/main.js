03bb6cc2d24c583b58959c631ba1465e9587f155
AriaPersadantaGinting <persadanta13@gmail.com>
2024-10-04 08:59:23 +0700

    complete

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import { app } from "./application/app.js";
 import { configDotenv } from "dotenv";
 import { logger } from "./application/logger.js";
+import { createServer } from "http";
 
 configDotenv();
 
 const port = process.env.PORT;
+
+// Export a function handler for Vercel
+const server = createServer(app);
+
+export default (req, res) => {
+  server.emit("request", req, res);
+};
 
 app.listen(port, () => {
   logger.info(`Server is running on port ${port}!`);
