1777db732f0e286fe72f0df026eb01a3a888e5be
AriaPersadantaGinting <persadanta13@gmail.com>
2024-10-04 11:22:21 +0700

    supabase changes

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://sfsfgqvuvbeqdjnewgpy.supabase.co";
-const supabaseKey = process.env.ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
